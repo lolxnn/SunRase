cb89ea7536b5202e64e4ca0e36e333860e326d2b
lolxnn <inkyincolto@gmail.com>
2018-02-27 18:29:24 +0100

    Aggiunto avvio motore brushless
    (da testare)

--- SunRase4.0_lcd.ino
+++ SunRase4.0_lcd.ino
@@ -3,13 +3,29 @@
  * Code by: Reolfi Riccardo & Incolto Davide.
 */
 #include <LiquidCrystal.h>
+#include <Servo.h>
+Servo motor_1;
 const int tempPin = A0;
 const int batPin = A1;
 LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
 void setup() {
-  lcd.begin(16, 2);
+  motor_1.attach(9);
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
+  pinMode(A3, INPUT);
+  lcd.begin(16, 2);
+  lcd.print("Avvio Motori");
+  int velocitaBrushless = map(analogRead(A3), 0, 1023, 100, 150);
+  delay(1000);
+  for(int i = 80; i < velocitaBrushless; i++){
+    lcd.setCursor(0, 1);
+    lcd.print("Stato:", velocitaBrushless, 0, 100),"%")
+    motor_1.write(i);
+    delay(10);
+    lcd.clear();
+  }
+  lcd.clear();
+  
 }
 
 void loop() {
